b5eb6c4f4f388e6991c16ffd63076de7496f481e
eligijus <elasorama@gmail.com>
2022-02-27 13:21:43 +0200

    Added the X and Y split to the pipeline

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,6 +1,8 @@
 # Importing all the methods for the pipeline 
+from venv import create
 from pipeline.modules.read_data import read_json
 from pipeline.modules.clean_data import clean_text
+from pipeline.modules.model_input_preparation import create_X_Y, apply_train_test_split
 
 # Directory traversal 
 import os 
@@ -17,3 +19,10 @@ if __name__ == "__main__":
 
     # Cleaning the reviewText column 
     d['reviewText'] = [clean_text(x) for x in d['reviewText']]
+
+    # Creating the training and testing sets
+    train, test = apply_train_test_split(d, test_size=0.2, random_seed=42)
+
+    # Creating the X and Y matrices
+    X_train, Y_train = create_X_Y(train, x_col='reviewText', y_col='overall')
+    X_test, Y_test = create_X_Y(test, x_col='reviewText', y_col='overall')
